ef3bb31f718837c5467880835ba8e74e0d937f42
sujan-shetty71 <113658538+sujan-shetty71@users.noreply.github.com>
2022-10-23 15:44:04 +0530

    Delete newele directory

--- newele/migrations/2_ele_comm.js
+++
@@ -1,5 +0,0 @@
-const ElectionCommision = artifacts.require("ElectionCommision");
-
-module.exports = async function (deployer){
-   await deployer.deploy(ElectionCommision);
-};
--- newele/migrations/3_elec_2022.js
+++
@@ -1,5 +0,0 @@
-const Election2022 = artifacts.require("Election2022");
-
-module.exports = function (deployer) {
-  deployer.deploy(Election2022,"0x3ba579cEBb9F8C1342E6773593a58A0e7921bB13");
-};
